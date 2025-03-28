62e8c5ab2315ad4d7ac0396cbc49308e3d06ad13
redson158 <redson@ithaca.edu>
2025-03-28 10:48:45 -0400

    function headers and javadocs for business related classes

---
+++ src/main/java/Activity.java
@@ -0,0 +1,41 @@
+public class Activity {
+    private String name;
+    private String type;
+    private Business location;
+    private boolean active;
+
+    public Activity(String name, String type, Business location){
+        this.name = name;
+        this.type = type;
+        this.location = location;
+        this.active = true;
+    }
+
+    public String getName(){
+        throw new RuntimeException("not yet implemented");
+    }
+
+    public String getType(){
+        throw new RuntimeException("not yet implemented");
+    }
+
+    public Business getLocation(){
+        throw new RuntimeException("not yet implemented");
+    }
+
+    /**checks if activity is current
+     * input - none
+     * output - boolean
+     */
+    public boolean isActive(){
+        throw new RuntimeException("not yet implemented");
+    }
+
+    /**when owner removes activity from business, activity set to inactive
+     * input - none
+     * output - none, active set to false
+     */
+    public void endActivity(){
+        throw new RuntimeException("not yet implemented");
+    }
+}
--- src/main/java/layout/Business.java
+++ src/main/java/layout/Business.java
@@ -1,7 +1,56 @@
 package layout;
 
+
+
 public class Business extends POI{
-    public Business(String name, Terminal terminal){
+    private Activity activity;
+    private String type;
+    private String hours;
+
+    public Business(String name, Terminal terminal, String type){
         super(name, terminal);
+        this.activity = null;
+    }
+
+    public String getType(){
+        throw new RuntimeException("not yet implemented");
+    }
+
+    public String getHours(){
+        throw new RuntimeException("not yet implemented");
+    }
+
+    public Activity getActivity(){
+        throw new RuntimeException("not yet implemented");
+    }
+
+    /**checks if business already has activity and adds if not
+     * input - name and type of activity
+     * output - none, activity added to business
+     * @throws IllegalArgumentException if business already has activity
+     */
+    public void addActivity(String name, String type){
+        throw new RuntimeException("not yet implemented");
+    }
+
+    /**removes activity if business has one
+     * input - none
+     * output - none, activity removed from business
+     * @throws IllegalArgumentException if business does not have activity
+     */
+    public void removeActivity(){
+        throw new RuntimeException("not yet implemented");
+    }
+
+    /**checks if business currently has an activity
+     * input - none
+     * output - boolean
+     */
+    public boolean hasActivity(){
+        throw new RuntimeException("not yet implemented");
+    }
+
+    public void updateHours(String hours){
+        throw new RuntimeException("not yet implemented");
     }
 }
