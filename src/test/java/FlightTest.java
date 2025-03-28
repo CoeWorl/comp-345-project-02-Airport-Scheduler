0ffa25f055c34a858bc30a84a953516417151c41
CoeWorl <cole.a.wissink@gmail.com>
2025-03-28 10:34:00 -0400

    Changed FlightTest Setup

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,21 +1,16 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import layout.*;
+import org.junit.Test;
 
-import java.util.List;
+import java.io.IOException;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
 
 public class FlightTest {
     private Flight flight;
-
-    @BeforeEach
-    public void setUp() {
-        flight = new Flight("AA123", new Airport("JFK"), new Airport("LAX"), 1617225600, 1617232800, "On Time", new Terminal("A"), new Gate("A1"));
-    }
-
+    
     @Test
     public void changeStatusTest(){
+        flight = new Flight("AA123", new Airport("JFK"), new Airport("LAX"), 1617225600, 1617232800, "On Time", new Terminal("A"), new Gate("A1"));
         flight.changeStatus("Delayed");
         assertEquals("Delayed", flight.getStatus());
     }
