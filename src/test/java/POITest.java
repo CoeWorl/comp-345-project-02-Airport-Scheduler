63e4bce8266e8337ca622375a5f7828a78608ecf
oscar <ojimenezrodriguez@ithaca.edu>
2025-03-31 10:14:40 -0400

    finished poi class and tests

--- src/test/java/POITest.java
+++ src/test/java/POITest.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
 import layout.*;
 import org.junit.Test;
 
@@ -8,18 +9,11 @@ import static org.junit.Assert.*;
 public class POITest {
     @Test
     public void createPOITest() throws IOException {
-        POI poi1, poi2, poi3;
-        try{
-            poi1 = Json.fromJsonFile("src/test/resources/JFK/POI/Gate/aeebc26c-96cd-40a6-9055-9b2a164db481.json", Gate.class);
-            poi2 = Json.fromJsonFile("src/test/resources/JFK/POI/Gate/acace207-152a-4042-85f6-b43234da2417.json", Gate.class);
-            poi3 = Json.fromJsonFile("src/test/resources/JFK/POI/Business/b624702e-eb27-4392-817a-2e0cc01c28e1.json", Business.class);
-        }
-        catch(IOException e){
-            poi1 = null;
-            poi2 = null;
-            poi3 = null;
-            System.err.println(e);
-        }
+        POI poi1, poi2, poi3, poi4;
+
+        poi1 = Json.fromJsonFile("src/test/resources/JFK/POI/Gate/aeebc26c-96cd-40a6-9055-9b2a164db481.json", Gate.class);
+        poi2 = Json.fromJsonFile("src/test/resources/JFK/POI/Gate/acace207-152a-4042-85f6-b43234da2417.json", Gate.class);
+        poi3 = Json.fromJsonFile("src/test/resources/JFK/POI/Business/b624702e-eb27-4392-817a-2e0cc01c28e1.json", Business.class);
 
         assert poi1 != null;
         assertEquals("South Entrance", poi1.getName());
@@ -31,6 +25,12 @@ public class POITest {
         assertEquals("Joe Pizza", poi3.getName());
 
         assertThrows(IOException.class, () -> Json.fromJsonFile("src/test/resources/JFK/POI/Gate/invalid.json", Gate.class));
+
+        // Shows that business could be created from Gate file
         Json.fromJsonFile("src/test/resources/JFK/POI/Gate/acace207-152a-4042-85f6-b43234da2417.json", Business.class);
+
+
+        assertThrows(InvalidDefinitionException.class, ()-> Json.fromJsonFile(
+                "src/test/resources/JFK/POI/Business/b624702e-eb27-4392-817a-2e0cc01c28e1.json", POI.class));
     }
 }
