2c1ab6b62c663c9bdb2dedde5c885dd00da2c3d5
redson158 <redson@ithaca.edu>
2025-04-15 10:38:41 -0400

    added tests for schedule times

--- src/test/java/ScheduleTest.java
+++ src/test/java/ScheduleTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
@@ -24,5 +25,16 @@ public class ScheduleTest {
         assertEquals(terminal, schedule.getTerminal());
         assertEquals(schedule.getPOIs().size(), 1);
         assertEquals(schedule.getDepartureTime(), 1617225600);
+    }
+
+    @Test
+    public void timeTest(){
+        Airport airport = new Airport("JFK", "John F. Kennedy International Airport");
+        Terminal terminal = new Terminal("B", 2, null, "JFK");
+        Schedule schedule = new Schedule(1744799400, airport, terminal);
+        schedule.getDeptTime(); //April 16, 2025 @ 10:30:00 (UTC)
+        String expected = "19 hours and 53 minutes until departure" //as of April 15, 2025 @ 14:34:00 (UTC)
+        assertEquals(schedule.timeUntilDeparture(), expected);
+        schedule.timeToArriveAtAirport(); //April 16, 2025 @ 7:30:00 (UTC)
     }
 }
