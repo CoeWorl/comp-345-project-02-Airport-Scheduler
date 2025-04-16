474861d9babdfccbcf1db1f20a3441a68a34be51
oscar <ojimenezrodriguez@ithaca.edu>
2025-04-16 16:13:16 -0400

    no duplicate object detection implemented!

--- src/test/java/TerminalTest.java
+++ src/test/java/TerminalTest.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class TerminalTest {
     @Test
