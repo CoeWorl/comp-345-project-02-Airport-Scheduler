e3f2eb1b3d733f951aece629685f3c68f2830a9e
oscar <ojimenezrodriguez@ithaca.edu>
2025-04-17 16:48:39 -0400

    tweaking to avoid special characters to LLM

--- src/main/java/users/OllamaClient.java
+++ src/main/java/users/OllamaClient.java
@@ -1,5 +1,8 @@
 package users;
 
+import layout.Json;
+import layout.Terminal;
+
 import java.io.IOException;
 import java.net.http.*;
 import java.net.URI;
@@ -7,22 +10,22 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 
 public class OllamaClient {
-    public static List<Activity> plannerRequest(User user) throws IOException, InterruptedException {
-        String systemPrompt = "For the following topic and difficulty (on a scale of 1 to 5), " +
-                "generate an original coding problem, language agnostic." +
-                "For example: example input: 'arrays 3'" +
-                "example output: 'Write a function that takes an array of integers and returns the sum of all even numbers.'" +
-                "Please provide a similar output for the input below, and do not include any additional text.";
+    public static String plannerRequest(User user, Terminal t, List<Activity> activities) throws IOException, InterruptedException {
+        String systemPrompt = "For the following preferences of a User and available activities at airport, give an appropriate ranked recommendation" +
+                "Provide a list of activities with the following format: " +
+                "activity name, activity type, activity location ; activity2 name, activity2 type, activity2 location ; activity3 name, activity3 type, activity3 location ; " +
+                "I will give you Json files for the user, the terminal with its weighted graph of Businesses and the list of activities at the airport.";
 
         String json = String.format("""
         {
         "model": "llama3.1:8b",
             "messages": [
-                { "role": "user", "content": "%s %s %d"}
+                { "role": "user", "content": "%s %s %s %s"}
             ]
         }
-        """, systemPrompt);
-
+        """, systemPrompt, Json.toJsonString(user), Json.toJsonString(t), Json.toJsonString(activities));
+        json = json.replace("\r", "");
+        json = json.replace("\n", "");
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://localhost:11434/api/chat"))
@@ -31,7 +34,7 @@ public class OllamaClient {
                 .build();
 
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String parsedResponse = OllamaResponseParser.parseResponse(response.body());
-        return null;
+        System.err.println(response.body());
+        return OllamaResponseParser.parseResponse(response.body());
     }
 }
--- src/test/java/LLMTest.java
+++ src/test/java/LLMTest.java
@@ -1,15 +1,25 @@
+import layout.Business;
 import layout.Json;
+import layout.Terminal;
 import org.junit.Test;
+import users.Activity;
 import users.OllamaClient;
 import users.Passenger;
 import users.User;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class LLMTest {
     @Test
-    public void testLLM() throws IOException {
+    public void testLLM() throws IOException, InterruptedException {
         User rebecca = Json.fromJsonFile("src/test/resources/rebeccaNoFlights.json", Passenger.class);
         System.err.println(rebecca.getBeverage_preferences());
+        Terminal t = Json.fromJsonFile("src/test/resources/JFK/1.json", Terminal.class);
+        Activity a = new Activity("Pizza 50% Off", "Food", (Business) t.getPOI(UUID.fromString("b624702e-eb27-4392-817a-2e0cc01c28e1")));
+        ArrayList<Activity> activities = new ArrayList<>();
+        activities.add(a);
+        System.out.println(OllamaClient.plannerRequest(rebecca, t, activities));
     }
 }
